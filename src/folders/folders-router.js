e467d3b7e5592eb5798df2d203ffeeaf35444dc0
Michael Oldacre <mloldacre@live.com>
2020-08-25 15:51:38 -0400

    Folder GET and POST working

--- src/folders/folders-router.js
+++ src/folders/folders-router.js
@@ -21,6 +21,31 @@ foldersRouter
         res.json(folders.map(serializerFolder));
       })
       .catch(next);
+  })
+  .post(jsonParser, (req, res, next) => {
+    const { folder_name } = req.body;
+    const newFolder = { folder_name };
+
+    for (const [key, value] of Object.entries(newFolder)) {
+      // eslint-disable-next-line eqeqeq
+      if (value == null) {
+        return res.status(400).json({
+          error: { message: `Missing '${key}' in request body` }
+        });
+      }
+    }
+
+    folderService.insertFolder(
+      req.app.get('db'),
+      newFolder
+    )
+      .then(folder => {
+        res
+          .status(201)
+          .location(path.posix.join(req.originalUrl, `/${folder.id}`))
+          .json(serializerFolder(folder));
+      })
+      .catch(next);
   });
 
 module.exports = foldersRouter;
